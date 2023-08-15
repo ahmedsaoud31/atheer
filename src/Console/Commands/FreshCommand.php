4c9ba782adc922c918be81cbb0e9910e5c72da4d
Ahmed Aboelsaoud <ahmedsaoud31@gmail.com>
2023-08-15 07:50:15 +0300

    Update FreshCommand.php
    Add backup before migrate:fresh and seed backup data after migration.

--- src/Console/Commands/FreshCommand.php
+++ src/Console/Commands/FreshCommand.php
@@ -4,6 +4,9 @@ namespace Atheer\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
 
 use Database\Seeders\AtheerSeeder;
  
@@ -28,10 +31,46 @@ class FreshCommand extends Command
 	 */
 	public function handle(): void
 	{
-        if ($this->confirm("This command will delete all database data, are you sure you want to process this action?")) {
+        if ($this->confirm("This command will backup your data before re-migrate tables, are you sure you want to process this action?")) {
+            
+            $this->question('Please be patient while we backup your data and re-migrate tables and seed data again to your database.');
+            $this->backup();
             $this->call('migrate:fresh', []);
-            (new AtheerSeeder)->run();
+            $this->seed();
+            // (new AtheerSeeder)->run();
             $this->info('Fresh atheer done.');
         }
 	}
+
+    private function backup()
+    {
+        if(!Storage::disk('local')->exists('atheer')){
+            Storage::disk('local')->makeDirectory('atheer', 0775, true, true);
+        }
+        $tables = \DB::select('SHOW TABLES');
+        $tables = array_map('current',$tables);
+        foreach($tables as $table){
+            $data = \DB::table($table)->get()->toJson();
+            Storage::disk('local')->put("atheer/{$table}.json", $data);
+        }
+    }
+
+    private function seed()
+    {
+        Schema::disableForeignKeyConstraints();
+        $tables = \DB::select('SHOW TABLES');
+        $tables = array_map('current',$tables);
+        foreach($tables as $table){
+            if(in_array($table, ['migrations'])) continue;
+            $columns = Schema::getColumnListing($table);
+            $records = json_decode(Storage::disk('local')->get("atheer/{$table}.json"), true);
+            if($records){
+                foreach ($records as $record) {
+                    $filtered = Arr::only($record, $columns);
+                    \DB::table($table)->insert($filtered);
+                }
+            }
+        }
+        Schema::enableForeignKeyConstraints();
+    }
 }
